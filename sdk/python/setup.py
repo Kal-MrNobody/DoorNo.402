c7a89072aae9b7b9a3ce6fafd206b9367c6696ff
Kal-MrNobody <khushalshadija05@gmail.com>
2026-05-01 17:50:27 +0530

    bump python sdk to 0.3.0

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="doorno402",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=[
         "web3",
         "eth-account",
         "colorama",
+        "httpx",
+        "python-dotenv",
     ],
 )
