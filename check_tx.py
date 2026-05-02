a1a0ebabca744f8fe2caba8aab49db98c4938c3d
Kal-MrNobody <khushalshadija05@gmail.com>
2026-05-02 19:54:38 +0530

    clean root directory artifacts

--- check_tx.py
+++
@@ -1,4 +0,0 @@
-from web3 import Web3
-w3 = Web3(Web3.HTTPProvider("https://sepolia.base.org"))
-receipt = w3.eth.get_transaction_receipt("0xe48234441e6a29c1a68c3c5d2d3b90a4a838d93198a07f06123ad1d7fcab38e1")
-print(f"Status: {receipt.status}")
